cf51b620857292d60231eb274d173beb00d2d31b
unknown <advancenetprog@gmail.com>
2019-05-15 10:12:33 +0700

    first commit

---
+++ modules/dirlister.py
@@ -0,0 +1,8 @@
+import os
+
+def run(**args):
+    return str(os.listdir("."))
+
+# untuk bash $ls 
+# folder working directory
+# print(str(os.listdir(".")))
---
+++ modules/environment.py
@@ -0,0 +1,11 @@
+import os
+
+# args: argument system
+# argv: size of argument(s)
+
+def run(**args):
+    return str(os.environ)
+
+
+# print os environment
+# print str(os.environ)
